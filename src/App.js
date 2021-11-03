c91b2a1f4a41b8c3a04b044c25f3d3fcd00c4c92
bhtibrewal <42600164+bhtibrewal@users.noreply.github.com>
2021-11-03 16:18:56 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import "./styles.css";
+import { useState } from "react";
+var initialPrice, quantity, currentPrice;
+export default function App() {
+  const [output, setOutput] = useState(" ");
+
+  function calcLoss() {
+    var loss = (initialPrice - currentPrice) * quantity;
+    var lossPercent = (loss / initialPrice) * 100;
+    return [loss, lossPercent];
+  }
+  function calcProfit() {
+    var profit = (currentPrice - initialPrice) * quantity;
+    var profitPercent = (profit / initialPrice) * 100;
+    return [profit, profitPercent];
+  }
+  function clickHandler() {
+    if (initialPrice <= 0 || currentPrice <= 0 || quantity <= 0) {
+      alert("Please enter a valid price and quantity");
+    } else {
+      if (initialPrice > currentPrice) {
+        var loss = calcLoss();
+        setOutput(`Loss: ${loss[0]} Loss Percent: ${loss[1]}`);
+      } else if (initialPrice < currentPrice) {
+        var profit = calcProfit();
+        setOutput(`Profit: ${profit[0]} Profit Percent: ${profit[1]}`);
+      } else setOutput("No pain No gain!");
+    }
+  }
+
+  return (
+    <div className="App">
+      <h1>Stock Purchase</h1>
+      <div>
+        <label>
+          Initial Price
+          <input
+            type="number"
+            onChange={(e) => {
+              initialPrice = Number(e.target.value);
+            }}
+          />
+        </label>
+        <label>
+          Quantity of Stocks
+          <input
+            type="number"
+            onChange={(e) => {
+              quantity = Number(e.target.value);
+            }}
+          />
+        </label>
+        <label>
+          Current Price
+          <input
+            type="number"
+            onChange={(e) => {
+              currentPrice = Number(e.target.value);
+            }}
+          />
+        </label>
+      </div>
+      <button onClick={clickHandler}>Calculate</button>
+      <div>{output}</div>
+    </div>
+  );
+}
